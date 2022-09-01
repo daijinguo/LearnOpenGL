78b3f5e08f4135815ecde560b91eb5c87a4e3c03
daijinguo <daijinguo@agora.io>
2022-09-01 19:01:26 +0800

    demo 2-1 hello_triangle

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,16 @@
 
-#define PROJ_INSTANCE(use, name, argc, argv)  \
-    do {                                      \
-        if( (use) == false ) break;           \
-        extern int main_##name(int, char*[]); \
-        return main_##name(argc, argv);       \
-    } while(false)
+#define PROJ_INSTANCE(use, name, argc, argv)                                                                           \
+	do {                                                                                                               \
+		if ((use) == false)                                                                                            \
+			break;                                                                                                     \
+		extern int main_##name(int, char *[]);                                                                         \
+		return main_##name(argc, argv);                                                                                \
+	} while (false)
 
-int main(int argc, char *argv[]) {
-    PROJ_INSTANCE(true, 1_1, argc, argv);
+int main(int argc, char *argv[])
+{
+	PROJ_INSTANCE(false, 1_1, argc, argv);
+	PROJ_INSTANCE(true, 2_1, argc, argv);
 
-    return 0;
+	return 0;
 }
